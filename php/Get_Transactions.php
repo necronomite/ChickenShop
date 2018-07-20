a2dc976ad719507bcb2fbcfdc7b5ef6e1949e35c
sufera <joanahsanz@gmail.com>
2018-07-20 18:58:32 +0800

    createDB.php - changed dates of transactions to '2018-07-20'; Get_Transactions.php  - modified the queries with respect to DB structure changes, also this time, column "chicken_head" will be returned

--- php/Get_Transactions.php
+++ php/Get_Transactions.php
@@ -3,18 +3,16 @@ include 'DB_connector.php';
 
 
 // $date = date("Y-m-d", strtotime($_POST['date']."+1 days",)));
-$date = $_POST['date'];
-// $date = '2017-04-26';
+// $date = $_POST['date'];
+$date = '2018-07-20';
 
-$sql_get_transactions = "SELECT t.id, c.name, DATE_FORMAT(t.transaction_date, '%b %d, %Y') as transaction_date, sum(a.cost) as total, t.amount_paid, (sum(a.cost) - t.amount_paid) as balance 
+$sql_get_transactions = "SELECT t.id, c.name, DATE_FORMAT(t.transaction_date, '%b %d, %Y') as transaction_date, round(sum(a.cost), 2) as total, t.amount_paid, round((sum(a.cost) - t.amount_paid), 2) as balance 
         	FROM 
 		customers c, 
 		transactions t, 
-		(SELECT p.transaction_id as tid, i.name, p.quantity, p.unit_type AS unit, 'x' as ops, 
-			CASE p.unit_type WHEN 'kg' THEN i.kg_price WHEN 'pcs' THEN i.pcs_price END as price, round(p.quantity* 
-			CASE p.unit_type WHEN 'kg' THEN i.kg_price WHEN 'pcs' THEN i.pcs_price END, 2) as cost 
-         FROM purchases p, items i, transactions t 
-         WHERE p.item_id = i.id AND t.id = p.transaction_id) a 
+		(SELECT p.transaction_id as tid, p.quantity, p.rate, round(p.quantity*p.rate, 2) as cost 
+         FROM purchases p, transactions t 
+         WHERE t.id = p.transaction_id) a 
         WHERE t.customer_id = c.id AND t.id = a.tid AND t.transaction_date = '$date'
         GROUP BY t.id
 		";
@@ -25,7 +23,7 @@ while ($row = mysqli_fetch_assoc($result)) {
 	
 	$transaction_id = $row['id'];
 	
-	$sql_get_items  = "SELECT p.transaction_id as tid, i.name, p.quantity, p.unit_type AS unit,  'x' AS ops, CASE p.unit_type WHEN 'kg' THEN i.kg_price WHEN 'pcs' THEN i.pcs_price END as price, (p.quantity* CASE p.unit_type WHEN 'kg' THEN i.kg_price WHEN 'pcs' THEN i.pcs_price END) as cost FROM purchases p, items i, transactions t WHERE p.item_id = i.id AND t.id = p.transaction_id AND t.id = '$transaction_id'";
+	$sql_get_items  = "SELECT p.transaction_id as tid, i.name, p.quantity, 'x' AS ops, p.rate as price, (p.quantity*p.rate) as cost, p.chicken_head FROM purchases p, items i, transactions t WHERE p.item_id = i.id AND t.id = p.transaction_id AND t.id = '$transaction_id'";
 	
 	$items = array();
 	$items_sql_result = mysqli_query($conn,$sql_get_items);
@@ -36,6 +34,10 @@ while ($row = mysqli_fetch_assoc($result)) {
 	
 	$data[] = $row;
 }
-header('Content-Type: application/json');
-echo json_encode($data);
+echo "<pre>";
+print_r($data);
+
+
+// header('Content-Type: application/json');
+// echo json_encode($data);
 ?>
